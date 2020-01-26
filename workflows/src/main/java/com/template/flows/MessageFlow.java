5b38418e4cb7e957e5bd4baa4e2838fd9fe5bf52
Johan Sebastian Baldrich Hinestroza <tohoo@MacBook-de-Sebas.local>
2020-01-26 16:18:02 -0500

    Prueba de envío de mensajes y de recibir mensajes en Corda

---
+++ workflows/src/main/java/com/template/flows/MessageFlow.java
@@ -0,0 +1,66 @@
+package com.template.flows;
+
+import co.paralleluniverse.fibers.Suspendable;
+import com.template.contracts.TemplateContract;
+import com.template.states.MessageState;
+import net.corda.core.flows.*;
+import net.corda.core.utilities.ProgressTracker;
+
+// Add these imports:
+import net.corda.core.contracts.Command;
+import net.corda.core.identity.Party;
+import net.corda.core.transactions.SignedTransaction;
+import net.corda.core.transactions.TransactionBuilder;
+
+// Replace Initiator's definition with:
+@InitiatingFlow
+@StartableByRPC
+public class MessageFlow extends FlowLogic<Void> {
+    private final String message;
+    private final Party otherParty;
+
+    /**
+     * The progress tracker provides checkpoints indicating the progress of the flow to observers.
+     */
+    private final ProgressTracker progressTracker = new ProgressTracker();
+
+    public MessageFlow(String message, Party otherParty) {
+        this.message = message;
+        this.otherParty = otherParty;
+    }
+
+    @Override
+    public ProgressTracker getProgressTracker() {
+        return progressTracker;
+    }
+
+    /**
+     * The flow logic is encapsulated within the call() method.
+     */
+    @Suspendable
+    @Override
+    public Void call() throws FlowException {
+        // We retrieve the notary identity from the network map.
+        Party notary = getServiceHub().getNetworkMapCache().getNotaryIdentities().get(0);
+
+        // We create the transaction components.
+        MessageState outputState = new MessageState(message, getOurIdentity(), otherParty);
+        Command command = new Command<>(new TemplateContract.Commands.Action(), getOurIdentity().getOwningKey());
+
+        // We create a transaction builder and add the components.
+        TransactionBuilder txBuilder = new TransactionBuilder(notary)
+                .addOutputState(outputState, TemplateContract.ID)
+                .addCommand(command);
+
+        // Signing the transaction.
+        SignedTransaction signedTx = getServiceHub().signInitialTransaction(txBuilder);
+
+        // Creating a session with the other party.
+        FlowSession otherPartySession = initiateFlow(otherParty);
+
+        // We finalise the transaction and then send it to the counterparty.
+        subFlow(new FinalityFlow(signedTx, otherPartySession));
+
+        return null;
+    }
+}
---
+++ workflows/src/main/java/com/template/flows/MessageFlowResponder.java
@@ -0,0 +1,25 @@
+package com.template.flows;
+
+import co.paralleluniverse.fibers.Suspendable;
+import net.corda.core.flows.*;
+
+// ******************
+// * Responder flow *
+// ******************
+// Replace Responder's definition with:
+@InitiatedBy(MessageFlow.class)
+public class MessageFlowResponder extends FlowLogic<Void> {
+    private final FlowSession otherPartySession;
+
+    public MessageFlowResponder(FlowSession otherPartySession) {
+        this.otherPartySession = otherPartySession;
+    }
+
+    @Suspendable
+    @Override
+    public Void call() throws FlowException {
+        subFlow(new ReceiveFinalityFlow(otherPartySession));
+
+        return null;
+    }
+}
